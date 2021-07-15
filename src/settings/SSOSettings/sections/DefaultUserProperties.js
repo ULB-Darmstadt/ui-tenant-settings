25d2796ca2d6c9549e3d663ae0622dec811853d3
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2021-07-15 09:31:27 +0100

    feat: WIP - latest tenant changes

--- src/settings/SSOSettings/sections/DefaultUserProperties.js
+++ src/settings/SSOSettings/sections/DefaultUserProperties.js
@@ -5,35 +5,82 @@ import { Field, useFormState } from 'react-final-form';
 import {
   Col,
   Row,
-  TextField
+  TextField,
+  Select
 } from '@folio/stripes/components';
+import { useOkapiKy } from '@folio/stripes/core';
+import { useQuery } from 'react-query';
+
 //import stripesFinalForm from '@folio/stripes/final-form';
+
+const PARAMS_GROUPS = {
+  limit: 10000,
+  query: 'cql.allRecords=1 sortby group'
+};
+
+const PatronGroupSelection = ({defaultUserProp}) => {
   
+  const ky = useOkapiKy();
+  const { values } = useFormState();
+  const attr = {
+    disabled: !values['userCreateMissing'],
+    required: values['userCreateMissing']
+  };
+  
+  const { data = {} } = useQuery(
+    ['patronGroups'],
+    () => ky.get('groups', { searchParams: PARAMS_GROUPS }).json()
+  );
+  
+  const options = data?.usergroups?.map(g => ({
+    key: g.id,
+    value: g.id,
+    label: g.group.concat(g.desc ? ` (${g.desc})` : ''),
+  })) ?? [];
+  
+  return <Field
+    label={<FormattedMessage id="ui-tenant-settings.settings.saml.defaultUser.patronGroup" />}
+    name={`${defaultUserProp}.patronGroup`}
+    id="saml_patronGroup"
+    component={Select}
+    fullWidth
+    dataOptions={options}
+    defaultValue={values?.defaultUser?.patronGroup}
+    aria-required="true"
+    {...attr}
+  />;
+};
+
 const CustomTextField = ({input, label, id, required}) => {
   const { values } = useFormState();
-  return <TextField label={label} required={required} disabled={!values['userCreateMissing']} id={id} key={id} {...input} />
+  const attr = {
+    disabled: !values['userCreateMissing'],
+    required: (required && values['userCreateMissing'])
+  };
+  return <TextField label={label} id={id} key={id} {...attr} {...input} />
 };
 
 const CustomROTextField = ({input, label, id, required}) => {
   const { values } = useFormState();
   const {value, ...inputProps} = input;
-  return <TextField id={id} key={id}  required={required} readOnly disabled label={label} value={values['samlAttribute']} {...inputProps} />
+  return <TextField id={id} key={id} required={(required && values['userCreateMissing'])}
+    readOnly disabled label={label} value={values['samlAttribute']} {...inputProps} />
 };
 
 const PropertyCollectionSet = ({defaultUserProp, name}) => {
-  return <Row key={`saml_${name}Row`}>
-    <Col xs={12} md={6}>
+  return <>
+    <Col key={`saml_${name}Attribute`} xs={12} md={6}>
       <Field
-        label={<FormattedMessage id={`ui-tenant-settings.settings.saml.${name}Attribute`} />}
+        label={<FormattedMessage id={`ui-tenant-settings.settings.saml.defaultUser.${name}Attribute`} />}
         name={ `${defaultUserProp}.${name}Attribute` }
         id={`saml_${name}Attribute`}
         component={CustomTextField}
         fullWidth
       />
     </Col>
-    <Col xs={12} md={6}>
+    <Col key={`saml_${name}Default`} xs={12} md={6}>
       <Field
-        label={<FormattedMessage id={`ui-tenant-settings.settings.saml.${name}Default`} />}
+        label={<FormattedMessage id={`ui-tenant-settings.settings.saml.defaultUser.${name}Default`} />}
         name={ `${defaultUserProp}.${name}Default` }
         id={`saml_${name}Default`}
         component={CustomTextField}
@@ -41,8 +88,11 @@ const PropertyCollectionSet = ({defaultUserProp, name}) => {
         fullWidth
       />
     </Col>
-  </Row>
+  </>
 };
+
+  
+const propertySets = ['firstName', 'lastName'];
 
 const DefaultUserProperties = ({ defaultUserProp }) => {
   
@@ -55,39 +105,35 @@ const DefaultUserProperties = ({ defaultUserProp }) => {
   const usingUsername = enabled && currentUserProperty === 'username';
   const usingEmail = enabled && currentUserProperty === 'email';
   
-  const propertySets = ['firstName', 'lastName'];
-  
-  return <>
-    <Row>
-      <Col xs={12} >
-        <Field
-          label={<FormattedMessage id="ui-tenant-settings.settings.saml.usernameAttribute" />}
-          name={ `${defaultUserProp}.usernameAttribute` }
-          id="saml_usernameAttribute"
-          component={ !usingUsername ? CustomTextField : CustomROTextField }
-          initialValue={usingUsername ? currentSamlProperty : undefined}
-          fullWidth
-        />
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={12} >
-        <Field
-          label={<FormattedMessage id="ui-tenant-settings.settings.saml.emailAttribute" />}
-          name={ `${defaultUserProp}.emailAttribute` }
-          id="saml_emailAttribute"
-          component={ !usingEmail ? CustomTextField : CustomROTextField }
-          fullWidth
-        />
-      </Col>
-    </Row>
+  return <Row tagName='fieldset' >
+    <Col xs={12} tagName='legend' ><FormattedMessage id="ui-tenant-settings.settings.saml.defaultUserAttributes" /></Col>
+    <Col xs={12} >
+      <Field
+        label={<FormattedMessage id="ui-tenant-settings.settings.saml.defaultUser.usernameAttribute" />}
+        name={ `${defaultUserProp}.usernameAttribute` }
+        id="saml_usernameAttribute"
+        component={ !usingUsername ? CustomTextField : CustomROTextField }
+        initialValue={usingUsername ? currentSamlProperty : undefined}
+        fullWidth
+      />
+    </Col>
+    <Col xs={12} >
+      <Field
+        label={<FormattedMessage id="ui-tenant-settings.settings.saml.defaultUser.emailAttribute" />}
+        name={ `${defaultUserProp}.emailAttribute` }
+        id="saml_emailAttribute"
+        component={ !usingEmail ? CustomTextField : CustomROTextField }
+        initialValue={ usingEmail ? currentSamlProperty : undefined }
+        fullWidth
+      />
+    </Col>
     { propertySets.map(property => <PropertyCollectionSet key={`${property}Set` } defaultUserProp={defaultUserProp} name={property} />) }
-  </>
+    <Col xs={12} md={6}><PatronGroupSelection /></Col>
+  </Row>
 };
 
-
 DefaultUserProperties.propTypes = {
-  initialValues: PropTypes.string.isRequired,
+  defaultUserProp: PropTypes.string.isRequired,
 };
 
 export default DefaultUserProperties;
